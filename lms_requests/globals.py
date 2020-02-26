73b04e50e1c6c4d4b6f595057e315db6c24ff626
Asliddin Abdivasiyev <space.coding.programmer@gmail.com>
2020-02-26 15:42:53 +0500

    123

--- lms_requests/globals.py
+++ lms_requests/globals.py
@@ -2,6 +2,6 @@ from telebot import TeleBot
 
 schedule_url = 'https://lms.tuit.uz/faculty/student/schedule'
 bot_token = '989377982:AAEg3aO_psC91kurV9dQeOhp_7U_tQDo92I'
-heroku_url = 'https://hidden-forest-78520.herokuapp.com/'
+heroku_url = 'https://lms-schedule-bot.appspot.com/'
 admin = 898557820
 bot = TeleBot(bot_token)
